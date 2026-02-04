c8316a22534fa1c6bb00830fb19dc1163e531874
maximkrouk <id.maximkrouk@gmail.com>
2026-02-04 08:05:41 +0100

    feat: Update Package.swift
    - Use Swift 6.0 as base swift version
    - Add support for Swift 5.9

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -27,6 +27,5 @@ let package = Package(
 				.target(name: "Casification"),
 			]
 		),
-	],
-	swiftLanguageModes: [.v6]
+	]
 )
